9dc0779ed74ef508c8f65cef346d8a82cfc602b5
Facundo Berges <facundo.h.berges@gmail.com>
2023-04-22 10:48:02 -0300

    Agregando ejercicios hechos de los temas de la clase 1

---
+++ Ejercicio_01/Ejercicio_01/Ejercicio_01.cs
@@ -0,0 +1,64 @@
+﻿namespace Ejercicio_01
+{
+    /**
+     * 1.	Ingresar 5 números por consola, guardándolos en una variable escalar. 
+     * Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio.
+     * 
+     * */
+    internal class Ejercicio_01
+    {
+        static void Main(string[] args)
+        {
+            int numeroIngresado;
+            int max = 0;
+            int min = 0;
+            int total = 0;
+            double average;
+            string aux;
+
+            for (int i = 0; i < 5; i++)
+            {
+                do
+                {
+                    Console.Write($"Ingrese el numero {i+1}: ");
+                    aux = Console.ReadLine();
+                } while ( ! int.TryParse(aux, out numeroIngresado) );
+
+                total += numeroIngresado;
+
+                if (i == 0)
+                {
+                    max = numeroIngresado;
+                    min = numeroIngresado;
+                }
+
+                if (numeroIngresado > max)
+                {
+                    max = numeroIngresado;
+                }
+
+                if (numeroIngresado < min)
+                {
+                    min = numeroIngresado;
+                }
+            }
+            Console.WriteLine();
+
+            average = (double) total / 5;
+
+            Console.WriteLine("Maximo numero ingresado: {0}", max);
+            Console.WriteLine("Minimo numero ingresado: {0}", min);
+            Console.WriteLine("Total de los numeros ingresados: {0}", total);
+            Console.WriteLine("Promedio de los numeros ingresados: {0:N2}", average);
+
+            /*
+             * Console.WriteLine($"Maximo numero ingresado: {max}");
+             * Console.WriteLine($"Minimo numero ingresado: {min}");
+             * Console.WriteLine($"Total de los numeros ingresados: {total}");
+             * Console.WriteLine($"Promedio de los numeros ingresados: {average}");
+             * 
+             * 
+             */
+        }
+    }
+}
---
+++ Ejercicio_03/Ejercicio_03/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Ejercicio_03
+{
+    /**
+     * Mostrar por pantalla todos los números primos que haya hasta el número que 
+     * ingrese el usuario por consola.
+     * 
+     * Validar que el dato ingresado por el usuario sea un número.
+     * 
+     * Volver a pedir el dato hasta que sea válido o el usuario ingrese "salir". 
+     * Si ingresa "salir", cerrar la consola.
+     * 
+     * Al finalizar, preguntar al usuario si desea volver a operar. Si la respuesta es afirmativa, iterar. De lo contrario, cerrar la consola.
+     * 
+     * Nota: Utilizar estructuras repetitivas, selectivas y la función módulo (%).
+     */
+    internal class Ejercicio_03
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio_03";
+
+            string mensaje = "Ingrese un numero, o salir para finalizar el programa: ";
+            string? aux;
+            int num = 0;
+
+            do
+            {
+                Console.Write(mensaje);
+                aux = Console.ReadLine();
+
+                if(aux.Trim().ToLower() == "salir")
+                {
+                    break;
+                }
+
+                while( ! int.TryParse(aux, out num) || num <= 0)
+                {
+                    Console.Write("ERROR! Debe ingresar un numero positivo. {0}", mensaje);
+                    aux = Console.ReadLine();
+                }
+
+
+                Console.WriteLine("\nLos numeros primos entre 1 y el numero {0} son:", num);
+                for (int i = 2; i < num; i++)
+                {
+                    if ( esPrimo(i) )
+                    {
+                        Console.Write($"{i} ");
+                    }
+                }
+                Console.WriteLine();
+
+            } while (aux.Trim().ToLower() != "salir");
+
+        }
+
+        static bool esPrimo(int num)
+        {
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
