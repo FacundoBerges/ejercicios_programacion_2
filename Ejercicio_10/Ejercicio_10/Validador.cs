a9f51825fe5e860cbf33741cd228f710f396d530
Facundo Berges <facundo.h.berges@gmail.com>
2023-04-22 21:38:07 -0300

    update ejercicios

---
+++ Ejercicio_10/Ejercicio_10/Validador.cs
@@ -0,0 +1,23 @@
+﻿namespace Ejercicio_10
+{
+    internal class Validador
+    {
+        /// <summary>
+        /// Metodo que recibe un valor a validar, y valores minimo y maximo de rango. 
+        /// Devuelve true en caso de que se encuentre dentro del rango, y caso contrario, false.
+        /// </summary>
+        /// <param name="valor">Valor a validar</param>
+        /// <param name="min">Rango minimo permitido</param>
+        /// <param name="max"></param>
+        /// <returns>true si se encuentra dentro del rango min-max, caso contrario false</returns>
+        public static bool Validar(int valor, int min, int max)
+        {
+            return (valor > min) && (valor < max);
+        }
+
+        public static bool EsNumeroEntero(string valor)
+        {
+            return int.TryParse(valor, out _);
+        }
+    }
+}
